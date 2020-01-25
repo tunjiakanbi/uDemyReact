e720ed8e90049a78050e2ca41ff8071a83472d7a
tunjiakanbi <tunjiakanbi@gmail.com>
2020-01-25 17:17:00 -0600

    first JSX expression added

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -1,1 +1,9 @@
 console.log("app.js is running");
+//JSX is JavaScript XML
+// var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+    "h1", 
+    {id: "someid"}, 
+    "This is JSX from app.js");
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
