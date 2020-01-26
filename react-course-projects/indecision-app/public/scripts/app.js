67be2b26dd34d6eab93f61d074148e164819a8ee
tunjiakanbi <tunjiakanbi@gmail.com>
2020-01-25 19:11:23 -0600

    testing variables and objects with JSX

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -3,9 +3,18 @@
 console.log("app.js is running");
 //JSX is JavaScript XML
 var template = React.createElement(
-  "h1",
-  { id: "someid" },
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        { id: "someid" },
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some text"
+    )
 );
 // yarn init
 // yarn add babel-preset-react@6.24.1 babel-preset-env@1.5.2
@@ -15,5 +24,44 @@ var template = React.createElement(
 //     {id: "someid"}, 
 //     "This is JSX from app.js");
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+// var userName = 'Tunji Akanbi';
+// var userAge = 32;
+// var userLocation = "Rosemount MN";
+
+var user = {
+    name: 'Tunji Akanbi',
+    age: 42,
+    location: 'Rosemount, MN'
+};
+var templateTwo =
+// <div>
+// <h1>{userName}</h1>
+// <p>Age: {userAge}</p>
+// <p>Location: {userLocation}</p>
+// </div>
+
+React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
+ReactDOM.render(templateTwo, appRoot);
 console.log("app.js fixed");
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,6 +1,11 @@
 console.log("app.js is running");
 //JSX is JavaScript XML
- var template = <h1 id="someid">Indecision App</h1>;
+ var template = (
+     <div>
+     <h1 id="someid">Indecision App</h1>
+     <p>This is some text</p>
+     </div>
+ );
 // yarn init
 // yarn add babel-preset-react@6.24.1 babel-preset-env@1.5.2
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
@@ -9,5 +14,29 @@ console.log("app.js is running");
 //     {id: "someid"}, 
 //     "This is JSX from app.js");
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+// var userName = 'Tunji Akanbi';
+// var userAge = 32;
+// var userLocation = "Rosemount MN";
+
+var user = {
+    name: 'Tunji Akanbi',
+    age: 42,
+    location: 'Rosemount, MN'
+};
+var templateTwo = (
+    // <div>
+    // <h1>{userName}</h1>
+    // <p>Age: {userAge}</p>
+    // <p>Location: {userLocation}</p>
+    // </div>
+
+    <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+    </div>
+);
+
+ReactDOM.render(templateTwo, appRoot);
 console.log("app.js fixed");
