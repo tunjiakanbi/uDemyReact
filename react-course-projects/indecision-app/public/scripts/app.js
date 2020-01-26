db3416bf4ddd243d9e338e0ecc62d316c3ec7e56
tunjiakanbi <tunjiakanbi@gmail.com>
2020-01-25 18:28:07 -0600

    changed h1 content to "Indecision App"

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -5,12 +5,11 @@ console.log("app.js is running");
 var template = React.createElement(
   "h1",
   { id: "someid" },
-  "This is JSX from app.js"
+  "Indecision App"
 );
 // yarn init
 // yarn add babel-preset-react@6.24.1 babel-preset-env@1.5.2
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-//yarn install (recreates node modules folder if deleted)
 // var template = React.createElement(
 //     "h1", 
 //     {id: "someid"}, 
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,6 +1,6 @@
 console.log("app.js is running");
 //JSX is JavaScript XML
- var template = <h1 id="someid">This is JSX from app.js</h1>;
+ var template = <h1 id="someid">Indecision App</h1>;
 // yarn init
 // yarn add babel-preset-react@6.24.1 babel-preset-env@1.5.2
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
